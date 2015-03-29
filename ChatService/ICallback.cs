2e4729b8aafd87f623ddcdc847e708130d7a0da0
Laurentiu-Cernescu <laurentiu2993@yahoo.com>
2015-03-29 15:40:44 +0300

    EF + Interfaces

---
+++ ChatService/ICallback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    interface ICallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void NotifyNewMessage(Message message);
+
+        [OperationContract(IsOneWay = true)]
+        void NotifyStatusChange(User user, Status newStatus);
+    }
+}
---
+++ ChatService/IChatService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    [ServiceContract(CallbackContract = typeof(ICallback))]
+    interface IChatService
+    {
+        Response Login(User user);
+        Response Logout(User user);
+
+        Response SendMessage(Message message);
+
+        User[] GetFriends(User user);
+
+        Message[] GetMessages(User owner, User partner);
+
+        Response AddFriend(User from, User to);
+
+        Response RemoveFriend(User from, User to);
+    }
+}
